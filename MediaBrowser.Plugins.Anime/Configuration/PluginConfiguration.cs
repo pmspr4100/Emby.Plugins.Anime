ed4b28b54c2a73f4acf963f9e7776aa298609d18
Luke Pulverenti <luke.pulverenti@gmail.com>
2019-02-26 13:19:41 -0500

    removed dead code

--- MediaBrowser.Plugins.Anime/Configuration/PluginConfiguration.cs
+++ MediaBrowser.Plugins.Anime/Configuration/PluginConfiguration.cs
@@ -29,22 +29,18 @@ namespace MediaBrowser.Plugins.Anime.Configuration
         : BasePluginConfiguration
     {
         public TitlePreferenceType TitlePreference { get; set; }
-        public bool AllowAutomaticMetadataUpdates { get; set; }
         public bool TidyGenreList { get; set; }
         public int MaxGenres { get; set; }
         public bool AddAnimeGenre { get; set; }
-        public bool UseAnidbOrderingWithSeasons { get; set; }
         public string MyAnimeList_API_Name { get; set; }
         public string MyAnimeList_API_Pw { get; set; }
         public int AniDB_wait_time { get; set; }
         public PluginConfiguration()
         {
             TitlePreference = TitlePreferenceType.Localized;
-            AllowAutomaticMetadataUpdates = true;
             TidyGenreList = true;
             MaxGenres = 5;
             AddAnimeGenre = true;
-            UseAnidbOrderingWithSeasons = false;
             MyAnimeList_API_Name = "";
             MyAnimeList_API_Pw = "";
             AniDB_wait_time = 0;
